821345d600463b2afff50c1501b5d1d7375a345b
belcho <hpbecraft@gmail.com>
2021-10-24 19:40:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+
+//functions
+function addTodo(event){
+    event.preventDefault();
+
+    //todo div
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //create li
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //add todo file to local drive
+    saveLocalTodos(todoInput.value);
+
+    //check mark button
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //check trash button
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append to list
+
+    todoList.appendChild(todoDiv);
+
+    //todo input value
+
+    todoInput.value = "";
+
+
+
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //delete todo item
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        todo.classList.add('fall')
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+        
+    }
+
+    //check mark
+    if (item.classList[0] === "complete-btn") {
+
+    
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function saveLocalTodos(todo){
+    //check is there a todo file already?
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
